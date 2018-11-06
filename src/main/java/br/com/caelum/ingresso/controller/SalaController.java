02afecdc088e7c3048aa4cc7c3de646bcdb69810
osvaldo <lab7989@caelumBSB03-13>
2018-11-06 12:51:43 -0200

    Criando salas e sessoes

--- src/main/java/br/com/caelum/ingresso/controller/SalaController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SalaController.java
@@ -1,16 +1,19 @@
 package br.com.caelum.ingresso.controller;
 
-import br.com.caelum.ingresso.dao.SalaDao;
-import br.com.caelum.ingresso.model.Sala;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.validation.Valid;
-import java.util.Optional;
+import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Sala;
 
 /**
  * Created by nando on 03/03/17.
@@ -20,6 +23,9 @@ public class SalaController {
 
     @Autowired
     private SalaDao salaDao;
+    
+    @Autowired
+	private	SessaoDao sessaoDao;
 
 
     @GetMapping({"/admin/sala", "/admin/sala/{id}"})
@@ -33,21 +39,6 @@ public class SalaController {
         modelAndView.addObject("sala", sala);
 
         return modelAndView;
-    }
-
-
-
-
-    @PostMapping("/admin/sala")
-    @Transactional
-    public ModelAndView salva(@Valid Sala sala, BindingResult result){
-
-        if (result.hasErrors()){
-            return form(Optional.ofNullable(sala.getId()) ,sala);
-        }
-
-        salaDao.save(sala);
-        return new ModelAndView("redirect:/admin/salas");
     }
 
     @GetMapping("/admin/salas")
@@ -67,7 +58,7 @@ public class SalaController {
 
         ModelAndView view = new ModelAndView("sessao/lista");
         view.addObject("sala", sala);
-
+        view.addObject("sessoes", sessaoDao.buscaSessoesDaSala(sala));
         return view;
     }
 
