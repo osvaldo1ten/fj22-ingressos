7cc3e34b88c54266c5dac0dbe859985576516902
osvaldo <lab7989@caelumBSB03-13>
2018-11-08 11:17:13 -0200

    inclusao de preco da sala e do filme

--- src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.validacao;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Arrays;
@@ -16,16 +17,17 @@ import br.com.caelum.ingresso.model.Sessao;
 public class GerenciadorDeSessaoTest {
 
 	private Filme rogueOne;
-	private	Sala	sala3D;
-	private	Sessao	sessaoDasDez;
-	private	Sessao	sessaoDasTreze;
-	private	Sessao	sessaoDasDezoito;
+	private Sala sala3D;
+	private Sessao sessaoDasDez;
+	private Sessao sessaoDasTreze;
+	private Sessao sessaoDasDezoito;
+
 
 	@Before
 	public void preparaSessoes() {
-		this.rogueOne = new Filme("Rogue	One", Duration.ofMinutes(120), "SCI-FI");
-		this.sala3D = new Sala("Sala 3D");
-		this.sessaoDasDez = new Sessao(LocalTime.parse("10:00:00"), rogueOne, sala3D);
+		this.rogueOne = new Filme("Rogue	One", Duration.ofMinutes(120),	"SCI-FI", BigDecimal.ONE);
+		this.sala3D = new Sala("Sala	3D", BigDecimal.TEN);
+		this.sessaoDasDez = new Sessao(LocalTime.parse("10:00:00"), rogueOne,sala3D);
 		this.sessaoDasTreze = new Sessao(LocalTime.parse("13:00:00"), rogueOne,	sala3D);
 		this.sessaoDasDezoito = new Sessao(LocalTime.parse("18:00:00"),	rogueOne, sala3D);
 	}
